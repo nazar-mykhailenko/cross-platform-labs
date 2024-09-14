7037cbb1bab84b4da86bbc84f416899075ebbab9
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-09-14 12:04:51 +0300

    Lab1

---
+++ Lab1/Lab1/DominoHelper.cs
@@ -0,0 +1,25 @@
+namespace Lab1
+{
+    public static class DominoHelper
+    {
+        public static ulong CountDots(ulong n)
+        {
+            if (n > 10000)
+            {
+                throw new System.ArgumentException("n must be less than or equal to 10000");
+            }
+
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (n == 1)
+            {
+                return 3;
+            }
+
+            return Combinatorics.CombinationsWithRepetition(n + 1);
+        }
+    }
+}
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Lab1;
+
+const string FileName = "./INPUT.TXT";
+const string OutputFileName = "./OUTPUT.TXT";
+
+try
+{
+    string input = File.ReadAllText(FileName);
+    ulong n = ulong.Parse(input);
+    ulong result = DominoHelper.CountDots(n);
+    File.WriteAllText(OutputFileName, result.ToString());
+}
+catch (IOException)
+{
+    Console.WriteLine("Error when reading file!");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error when parsing input!");
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch (Exception)
+{
+    Console.WriteLine("Unknown error!");
+}
